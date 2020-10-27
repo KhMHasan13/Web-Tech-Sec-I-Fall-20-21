84ac00aec80a9fb38ffea534e4e753543a020f26
KhMHasan13 <hasan50cal@gmail.com>
2020-10-27 15:56:19 +0600

    Registration Form created

---
+++ Lab 03 Task/PHP Form Validation.php
@@ -0,0 +1,114 @@
+<html>
+	<head>PHP Form Validation</head>
+	
+	<body>
+	
+		<hr>
+	
+		<fieldset>
+			<legend><h1>Club Member Registration</h1></legend>
+			
+			<form>
+				<table>
+					<tr>
+						<td align = "right">Name:</td>
+						<td><input type = "text"></td>
+					</tr>
+					<tr>
+						<td align = "right">Username:</td>
+						<td><input type = "text"></td>
+					</tr>
+					<tr>
+						<td align = "right">Password:</td>
+						<td><input type = "text"></td>
+					</tr>
+					<tr>
+						<td align = "right">Confirm Password:</td>
+						<td><input type = "text"></td>
+					</tr>
+					<tr>
+						<td align = "right">Email:</td>
+						<td><input type = "text"></td>
+					</tr>
+					<tr>
+						<td align = "right">Phone:</td>
+						<td>
+							<input type = "text" size = "3" placeholder = "code">&nbsp;-&nbsp;
+							<input type = "text" size = "9" placeholder = "Number">
+						</td>
+					</tr>
+					<tr>
+						<td align = "right">Address:</td>
+						<td>
+							<input type = "text" placeholder = "Street Address"><br>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>
+							<input type = "text" size = "5" placeholder = "City">&nbsp;-&nbsp;
+							<input type = "text" size = "7" placeholder = "State">
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>
+							<input type = "text" placeholder = "Postal/Zip Code">
+						</td>
+					</tr>
+					<tr>
+						<td align = "right">Birth Date:</td>
+						<td>
+							<select>
+								<option disabled selected>Date</option>
+								<option></option>
+								<option></option>
+								<option></option>						
+							</select>
+							<select>
+								<option disabled selected>Month</option>
+								<option></option>
+								<option></option>
+								<option></option>						
+							</select>
+							<select>
+								<option disabled selected>Year</option>
+								<option></option>
+								<option></option>
+								<option></option>						
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td align = "right">Gender:</td>
+						<td>
+							<input type="radio" name="gender"> Male
+							<input type="radio" name="gender"> Female
+						</td>
+					</tr>
+					<tr>
+						<td align = "right">Where did you hear <br>about us?</td>
+						<td>
+							<input type="checkbox">A Friend or Colleague<br>
+							<input type="checkbox">Google<br>
+							<input type="checkbox">Blog Post<br>
+							<input type="checkbox">News Article
+						</td>
+					</tr>
+					<tr>
+						<td align = "right">Bio:</td>
+						<td>
+							<textarea></textarea>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" align="center"><input type="submit" value="register"></td>
+					</tr>
+				</table>
+			</form>
+			
+		</fieldset>
+	
+		</hr>
+	</body>
+</html>
