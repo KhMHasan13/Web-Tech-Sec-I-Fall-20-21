c58ef7fd9b7c2e9ccfcec4afb282ba4e2a2d3e1e
KhMHasan13 <hasan50cal@gmail.com>
2020-11-10 16:19:20 +0600

    first commit

---
+++ Lab 05 Task/login.php
@@ -0,0 +1,23 @@
+<html>
+	
+	<body>
+		<h3>Login</h3>
+		
+		<form>
+			<table>
+				<tr>
+					<td>Username:</td>
+					<td><input type = "text"></td>
+				</tr>
+				<tr>
+					<td>Password:</td>
+					<td><input type = "text"></td>
+				</tr>
+				<tr>
+					<td colspan = "2"><input type = "submit" value = "Login"></td>
+				</tr>
+			</table>
+		</form>
+		
+	</body>
+</html>
---
+++ Lab 06 Task/login.php
@@ -0,0 +1,38 @@
+<html>
+	<body>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		
+		
+		<form>
+			<table align = "center">
+				<tr align = "center">
+					<td><h2>&nbsp; &nbsp; LogIn</h2>
+				<tr>
+					<td>Username:</td>
+				</tr>
+				<tr>
+					<td><input type = "text" style = "height:30px"></td>
+				</tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr>
+					<td>Password:</td>
+				</tr>
+				<tr>
+					<td><input type = "text" style = "height:30px"></td>
+					<td><input type = "submit" value = "Login" style = "background-color:#85a16a;height:30px"></td>
+				</tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr>
+					<td><a href = "registration.php">Not registered yet?</a></td>
+				</tr>
+			</table>
+		</form>
+		
+	</body>
+</html>
